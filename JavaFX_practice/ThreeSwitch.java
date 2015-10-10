c3ec4229cefe9ff6a02b1e69a094feeb2b619209
KatherineB <kbiefer@golden.net>
2015-10-09 20:54:09 -0400

    adding circles

--- JavaFX_practice/ThreeSwitch.java
+++ JavaFX_practice/ThreeSwitch.java
@@ -15,18 +15,47 @@ import javafx.geometry.Pos;
 import javafx.stage.Stage;
  
 public class ThreeSwitch extends Application {
-	//int xp = 10;
-	//int yp = 10;
+	
     public static void main(String[] args) {
         launch(args);
     }
     
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("ThreeSwitch");
+        primaryStage.setTitle("ThreeSwitch");       
+     
+	    Group box = new Group();
 		
-        Button btnNew = new Button();
-        btnNew.setText("New Game");
+        Canvas holder = new Canvas(290, 50);
+        GraphicsContext gc = holder.getGraphicsContext2D();
+		drawFixedGraphics(gc);
+				
+		Canvas red1 = new Canvas(50, 50);
+		GraphicsContext gcRed1 = red1.getGraphicsContext2D();
+		drawRedCircle1(gcRed1);        
+				
+		Canvas blue1 = new Canvas(50, 50);
+		GraphicsContext gcBlue1 = blue1.getGraphicsContext2D();
+		drawBlueCircle1(gcBlue1);   
+
+        Canvas red2 = new Canvas(50, 50);
+		GraphicsContext gcRed2 = red2.getGraphicsContext2D();
+		drawRedCircle2(gcRed2);        
+				
+		Canvas blue2 = new Canvas(50, 50);
+		GraphicsContext gcBlue2 = blue2.getGraphicsContext2D();
+		drawBlueCircle2(gcBlue2); 	
+
+        Canvas red3 = new Canvas(50, 50);
+		GraphicsContext gcRed3 = red3.getGraphicsContext2D();
+		drawRedCircle3(gcRed3);        
+				
+		Canvas blue3 = new Canvas(50, 50);
+		GraphicsContext gcBlue3 = blue3.getGraphicsContext2D();
+		drawBlueCircle3(gcBlue3);		
+       
+	    Button btnNew = new Button();
+        btnNew.setText("Start Over");
         btnNew.setOnAction(new EventHandler<ActionEvent>() {
  
             @Override
@@ -44,31 +73,16 @@ public class ThreeSwitch extends Application {
                 System.out.println("Exit clicked");
             }
         });
-		
-		
-        
-      //  StackPane root = new StackPane();
-	    Group box = new Group();
-        Canvas holder = new Canvas(290, 50);
-        GraphicsContext gc = holder.getGraphicsContext2D();
-		
-		Canvas red1 = new Canvas(50, 50);
-		
-        GraphicsContext gcRed1 = red1.getGraphicsContext2D();
-        drawShapes(gc);
-		drawShapes2(gcRed1);
-        box.getChildren().add(holder);
-		box.getChildren().add(red1);
-       
-	   Button btnMoveLeft = new Button();
+	   
+	    Button btnMoveLeft = new Button();
         btnMoveLeft.setText("Move Left");
         btnMoveLeft.setOnAction(new EventHandler<ActionEvent>() {
  
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("test clicked");
-				//xp = xp + 10;
-				box.getChildren().remove(red3);
+				box.getChildren().remove(red1);
+				box.getChildren().add(blue1);
             }
         });
 		
@@ -79,15 +93,14 @@ public class ThreeSwitch extends Application {
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("test clicked");
-				//xp = xp + 10;
-				box.getChildren().add(red1);
+				box.getChildren().remove(red3);
+				box.getChildren().add(blue3);
             }
         });
 		
 		BorderPane gameBG = new BorderPane();
 		gameBG.setStyle("-fx-background-color: #aaaaaa;");
-		
-		
+				
 		FlowPane playArea = new FlowPane();
 		playArea.setStyle("-fx-background-color: #667733;");
 		FlowPane buttonRow = new FlowPane();
@@ -96,94 +109,71 @@ public class ThreeSwitch extends Application {
 		gameBG.setCenter(playArea);
 	    gameBG.setBottom(buttonRow);
 		
-		 playArea.getChildren().add(box);
+		playArea.getChildren().add(box);
 		playArea.setAlignment(Pos.CENTER);
+		
+		box.getChildren().add(holder);
+		box.getChildren().add(red1);
+		box.getChildren().add(red2);
+		box.getChildren().add(red3);
+		
         buttonRow.getChildren().add(btnNew);
 		buttonRow.getChildren().add(btnExit);
-		buttonRow.getChildren().add(btnTest);
+		buttonRow.getChildren().add(btnMoveLeft);
 		buttonRow.getChildren().add(btnTest2);
 		buttonRow.setAlignment(Pos.CENTER);
-		
-		
-	
+			
         primaryStage.setScene(new Scene(gameBG, 400, 150));
         primaryStage.show();
     }
-	private void drawShapes2(GraphicsContext gcRed1) {
-		gcRed1.setFill(Color.RED);
-	/*	gc2.fillRect(gc2.getCanvas().getLayoutX(),      
-                     gc2.getCanvas().getLayoutY(), 
-                     gc2.getCanvas().getWidth(), 
-                     gc2.getCanvas().getHeight());*/
-		gcRed1.fillOval(10, 10, 30, 30);			 
-	}
-	private void drawShapes(GraphicsContext gc) {
-		 gc.setFill(Color.TAN);
-		// gc.setStroke(Color.BLUE);
+	
+	private void drawFixedGraphics(GraphicsContext gc) {
+		gc.setFill(Color.TAN);
         gc.fillRect(gc.getCanvas().getLayoutX(),      
                      gc.getCanvas().getLayoutY(), 
                      gc.getCanvas().getWidth(), 
                      gc.getCanvas().getHeight());
 		gc.setFill(Color.GREY);
-      gc.fillOval(10, 10, 30, 30);
-	  gc.fillOval(50, 10, 30, 30);
-	  gc.fillOval(90, 10, 30, 30);
-	  gc.fillOval(130, 10, 30, 30);
-	  gc.fillOval(170, 10, 30, 30);
-	  gc.fillOval(210, 10, 30, 30);
-	  gc.fillOval(250, 10, 30, 30);
-	  gc.setFill(Color.RED);
-	//  gc.fillOval(10, 10, 30, 30);
+        gc.fillOval(10, 10, 30, 30);
+	    gc.fillOval(50, 10, 30, 30);
+	    gc.fillOval(90, 10, 30, 30);
+	    gc.fillOval(130, 10, 30, 30);
+	    gc.fillOval(170, 10, 30, 30);
+	    gc.fillOval(210, 10, 30, 30);
+	    gc.fillOval(250, 10, 30, 30);	 
+    }
+	private void drawRedCircle1(GraphicsContext gcRed1) {
+		gcRed1.setFill(Color.RED);
+		gcRed1.fillOval(10, 10, 30, 30);			 
+	}
+	private void drawBlueCircle1(GraphicsContext gcBlue1) {
+		gcBlue1.setFill(Color.BLUE);
+		gcBlue1.fillOval(10, 10, 30, 30);			 
+	}
+	private void drawRedCircle2(GraphicsContext gcRed2) {
+		gcRed2.setFill(Color.RED);
+		gcRed2.fillOval(50, 10, 30, 30);			 
+	}
+	private void drawBlueCircle2(GraphicsContext gcBlue2) {
+		gcBlue2.setFill(Color.BLUE);
+		gcBlue2.fillOval(50, 10, 30, 30);			 
+	}
+	private void drawRedCircle3(GraphicsContext gcRed3) {
+		gcRed3.setFill(Color.RED);
+		gcRed3.fillOval(90, 10, 30, 30);			 
+	}
+	private void drawBlueCircle3(GraphicsContext gcBlue3) {
+		gcBlue3.setFill(Color.BLUE);
+		gcBlue3.fillOval(90, 10, 30, 30);			 
+	}
+}
+/*
+ gc.setFill(Color.RED);
+	  gc.fillOval(10, 10, 30, 30);
 	  gc.fillOval(50, 10, 30, 30);
 	  gc.fillOval(90, 10, 30, 30);
 	  gc.setFill(Color.BLUE);
 	  gc.fillOval(170, 10, 30, 30);
 	  gc.fillOval(210, 10, 30, 30);
 	  gc.fillOval(250, 10, 30, 30);
-	 
-	//  gc.setLineWidth(5);
-	//  gc.strokeLine(40, 10, 10, 40);
-    //    gc.setLineWidth(3);
-   //     gc.strokeLine(80, 20, 20, 80);
-	//	gc.fillRoundRect(110, 60, 30, 30, 10, 10);
-	//	gc.fillArc(110, 110, 30, 30, 45, 240, ArcType.ROUND);
-		/*gc.setFill(Color.GREEN);
-        gc.setStroke(Color.BLUE);
-        gc.setLineWidth(5);
-        gc.strokeLine(40, 10, 10, 40);
-        gc.fillOval(10, 60, 30, 30);
-        gc.strokeOval(60, 60, 30, 30);
-        gc.fillRoundRect(110, 60, 30, 30, 10, 10);
-        gc.strokeRoundRect(160, 60, 30, 30, 10, 10);
-		 gc.fillArc(10, 110, 30, 30, 45, 240, ArcType.OPEN);
-        gc.fillArc(60, 110, 30, 30, 45, 240, ArcType.CHORD);
-        gc.fillArc(110, 110, 30, 30, 45, 240, ArcType.ROUND);
-        gc.strokeArc(10, 160, 30, 30, 45, 240, ArcType.OPEN);
-        gc.strokeArc(60, 160, 30, 30, 45, 240, ArcType.CHORD);
-        gc.strokeArc(110, 160, 30, 30, 45, 240, ArcType.ROUND);
-        gc.fillPolygon(new double[]{10, 40, 10, 40},
-                       new double[]{210, 210, 240, 240}, 4);
-        gc.strokePolygon(new double[]{60, 90, 60, 90},
-                         new double[]{210, 210, 240, 240}, 4);
-        gc.strokePolyline(new double[]{110, 140, 110, 140},
-                          new double[]{210, 210, 240, 240}, 4);*/
-		
-		/* gc.setFill(Color.WHITESMOKE);
-         gc.fillRect(gc.getCanvas().getLayoutX(),      
-                     gc.getCanvas().getLayoutY(), 
-                     gc.getCanvas().getWidth(), 
-                     gc.getCanvas().getHeight());
-         gc.setFill(Color.GREEN);
-         gc.setStroke(Color.BLUE);
-
-         gc.setLineWidth(5);
-         gc.strokeLine(40, 10, 10, 40);
-         gc.fillOval(10, 60, 30, 30);
-         gc.strokeOval(60, 60, 30, 30);
-         gc.fillRoundRect(110, 60, 30, 30, 10, 10);
-         gc.strokeRoundRect(160, 60, 30, 30, 10, 10);
-         gc.fillArc(10, 110, 30, 30, 45, 240, ArcType.OPEN);
-         gc.fillArc(60, 110, 30, 30, 45, 240, ArcType.CHORD);
-         gc.strokeArc(10, 160, 30, 30, 45, 240, ArcType.OPEN);*/
-    }
-}
+*/
