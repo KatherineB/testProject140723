d4e354029b3abcc883d6ed49c71cf84582dd3b9e
KatherineB <kbiefer@golden.net>
2015-11-21 21:11:23 -0500

    command line argument input implemented

--- C_practice/nicerJSON.c
+++ C_practice/nicerJSON.c
@@ -1,44 +1,82 @@
 #include <stdio.h>
 
-main()
+main(int argc, char *argv[])
 {
-
  char c;
  int numCharReq = 0;
  int leftSquareBracketCount = 0;
- int leftCurlyBracketCount = 0;
- int indentCount = 0;
+ 
+ if(argc == 2){
+         FILE *fp, *fopen();
+	 fp = fopen(argv[1],"r");
 
- FILE *fp, *fopen();
- fp = fopen("products.json","r");
-
- while(c != EOF && numCharReq < 1000 ){   
-	c = getc(fp);
-	if(c != EOF){   
-               switch(c){
-               case '[':
-                    leftSquareBracketCount++;
-                    doIndent(leftSquareBracketCount);
-                    printf("%c",c);
-                    break;
-               case ']':
-                    leftSquareBracketCount--;
-                    printf("%c",c);
-                    break;
-               case '{':
-                    doIndent(leftSquareBracketCount);
-                    printf("%c",c);
-                    break;
-               case ',':
-                    printf("%c",c);
-                    doIndent(leftSquareBracketCount);
-                    break;
-               default:
-                    printf("%c",c);
-               }
+         while(c != EOF){
+		c = getc(fp);
+		if(c != EOF){   
+		       switch(c){
+		       case '[':
+		            leftSquareBracketCount++;
+		            doIndent(leftSquareBracketCount);
+		            printf("%c",c);
+		            break;
+		       case ']':
+		            leftSquareBracketCount--;
+		            printf("%c",c);
+		            break;
+		       case '{':
+		            doIndent(leftSquareBracketCount);
+		            printf("%c",c);
+		            break;
+		       case ',':
+		            printf("%c",c);
+		            doIndent(leftSquareBracketCount);
+		            break;
+		       default:
+		            printf("%c",c);
+		       }
+		 }
+		 numCharReq++;    
 	 }
-	 numCharReq++;    
  }
+ else if(argc == 3){
+         FILE *fp, *fopen();
+	 fp = fopen(argv[1],"r");
+         int inputNumChar = atoi(argv[2]);
+
+	 while(c != EOF && numCharReq < inputNumChar){   
+		c = getc(fp);
+		if(c != EOF){   
+		       switch(c){
+		       case '[':
+		            leftSquareBracketCount++;
+		            doIndent(leftSquareBracketCount);
+		            printf("%c",c);
+		            break;
+		       case ']':
+		            leftSquareBracketCount--;
+		            printf("%c",c);
+		            break;
+		       case '{':
+		            doIndent(leftSquareBracketCount);
+		            printf("%c",c);
+		            break;
+		       case ',':
+		            printf("%c",c);
+		            doIndent(leftSquareBracketCount);
+		            break;
+		       default:
+		            printf("%c",c);
+		       }
+		 }
+		 numCharReq++;    
+	 }
+ }
+ else{
+     printf("Please enter the JSON file you wish to look at as a command line argument\n");
+     printf("or the file and an integer for the number of characters if you just want\n");
+     printf("to see a bit of the file without converting the whole thing.\n");
+ }
+
   printf("\n\n");
   printf("The number of characters was %d\n",numCharReq);
   printf("The number of left brackets was %d\n",leftSquareBracketCount);
