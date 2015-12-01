1397072de92c22b2fbc0f312899971ada460c90e
KatherineB <kbiefer@golden.net>
2015-11-30 19:44:41 -0500

    string being found with accurate count of number of times

--- C_practice/searchJOL.c
+++ C_practice/searchJOL.c
@@ -9,42 +9,40 @@ main(int argc, char *argv[])
  char line[200];
  char singleChar;
  int found = 0;
- char jsonKey1[] = "product_name";
- 
+ char jsonKey1[] = "10";
  
  if(argc == 2){
          FILE *fp, *fopen();
 	 fp = fopen(argv[1],"r");
          fgets(line, 200, fp);
          printf("%s\n",line);
-
      
       /*   printf("%c\n",singleChar); */
          
-       while(i<25 && found != 1){
+       while(i<140 /*&& found != 1*/){
               singleChar = line[i];
+              
+              printf("%d ",i);
               printf("%c\n",singleChar);
-              printf("%d\n",i);
-              i++;
-              if(singleChar == jsonKey1[0]){
+              
+              if(line[i] == jsonKey1[0]){
                    printf("in if\n");
-                   while(line[i+j] == jsonKey1[k]){
+                   j = i;
+                   while(line[j] == jsonKey1[k]){
+                          printf("j=%d k=%d letter=%c\n",j,k,line[j]);
                           j++;
-                          k++;
-                          printf("In If:%c\n",line[j]);
+                          k++;                       
                    }
-                   if(k == 12){
-                        found = 1;
-                   }
-                   else{
-                        j=0;
-                        k=0;
-                   }
+                   if(k > 1){
+                        found++;
+                   }                 
+                   j=0;
+                   k=0;                 
               }
-
+              i++;
          }
          
-         
+         printf("Number of Times String Occurs:%d\n",found);
  }
  else if(argc == 3){
       
