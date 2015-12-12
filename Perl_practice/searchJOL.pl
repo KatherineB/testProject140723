b0e0d9849bfcfb610320d518c71890b2c887f376
KatherineB <kbiefer@golden.net>
2015-12-11 19:51:01 -0500

    added arrays and loop to extract all json data from line

--- Perl_practice/searchJOL.pl
+++ Perl_practice/searchJOL.pl
@@ -4,6 +4,15 @@ use warnings;
 
 my $file_in = $ARGV[0];
 my $jsonKey1 = "product_name";
+my $jsonKey2 = "manufacturer";
+my @jsonKey = ("product_name","manufacturer","model","family");
+my @jsonData;
+my $divider = 3;
+my $startJK;
+my $startPadding;
+my $startJD;
+my $endJD;
+my $lengthJD;
 
 print $file_in . "\n";
 
@@ -12,12 +21,25 @@ open my $file, '<', $file_in;
 while(my $line = <$file>){
     my $currentLine = $line;
     print $currentLine . "\n";
-    my $location = index($currentLine,$jsonKey1);
-    print $location . "\n";
-    print length($jsonKey1). "\n";
-    my $offset = length($jsonKey1) + $location + 3;
-    my $limit = index($currentLine,"\"", $offset);
-    print $limit . "\n";
+
+    for (my $i = 0; $i < 4; $i++){
+	    $startJK = index($currentLine,$jsonKey[$i]);
+	    print $startJK . "\n";
+	    print length($jsonKey[$i]). "\n";
+
+	    $startPadding = $startJK;
+	    $startJD = length($jsonKey[$i]) + $startPadding + $divider;
+	    print $startJD . "\n";
+
+	    $endJD = index($currentLine,"\"", $startJD);
+	    print $endJD . "\n";
+
+	    $lengthJD = $endJD - $startJD;
+
+	    $jsonData[$i] = substr($currentLine,$startJD,$lengthJD);
+	    print $jsonData[$i] . "\n";
+    }
+
     last if $. == 1;
 }
 
