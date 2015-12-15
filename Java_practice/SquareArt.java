8d5f06c100269617a3fae921c6a8e8416fbc05b4
KatherineB <kbiefer@golden.net>
2015-12-14 20:27:40 -0500

    got coloured squares to appear properly in avail area

--- Java_practice/SquareArt.java
+++ Java_practice/SquareArt.java
@@ -1,3 +1,5 @@
+package squareartkb;
+
 import javax.swing.*;
 import javax.swing.border.*;
 import javax.imageio.*;
@@ -25,33 +27,41 @@ public class SquareArt extends JFrame{
 	   JPanel squaresAvail = new JPanel();
 	   squaresAvail.setLayout(new GridLayout(4,1));
 	   squaresAvail.setBackground(Color.decode("#55dd99"));
-	   background.add(squaresAvail, BorderLayout.EAST);
+	   squaresAvail.setPreferredSize(new Dimension(100,100));
+	   background.add(squaresAvail, BorderLayout.EAST);	   
 	   
 	   JPanel avail1 = new JPanel();
 	   avail1.setBackground(Color.decode("#ffffff"));
-	   avail1.setBorder(new LineBorder(Color.BLACK, 1));
+	   SquareArtSquare greenSquare = new SquareArtSquare("#00dd00");
+	   greenSquare.setPreferredSize(new Dimension(80,80));	
+       avail1.add(greenSquare);	   
 	   squaresAvail.add(avail1);
 	   
 	   JPanel avail2 = new JPanel();
 	   avail2.setBackground(Color.decode("#ffffff"));
-	   avail2.setBorder(new LineBorder(Color.BLACK, 1));
+	   SquareArtSquare blueSquare = new SquareArtSquare("#0000ff");
+	   blueSquare.setPreferredSize(new Dimension(80,80));
+	   avail2.add(blueSquare);
 	   squaresAvail.add(avail2);
 	   
 	   JPanel avail3 = new JPanel();
 	   avail3.setBackground(Color.decode("#ffffff"));
-	   avail3.setBorder(new LineBorder(Color.BLACK, 1));
+	   SquareArtSquare yellowSquare = new SquareArtSquare("#ffff00");
+	   yellowSquare.setPreferredSize(new Dimension(80,80));
+	   avail3.add(yellowSquare);
 	   squaresAvail.add(avail3);
 	   
 	   JPanel avail4 = new JPanel();
 	   avail4.setBackground(Color.decode("#ffffff"));
-	   avail4.setBorder(new LineBorder(Color.BLACK, 1));
+	   SquareArtSquare redSquare = new SquareArtSquare("#ff0000");
+	   redSquare.setPreferredSize(new Dimension(80,80));
+	   avail4.add(redSquare);
 	   squaresAvail.add(avail4);
 	  
 	   setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	 }
-     public static void main(String[]args){
-		 		  
-	   SquareArt screen = new SquareArt();
-	   
+	 
+     public static void main(String[]args){		 		  
+	   SquareArt screen = new SquareArt();  
 	 }
 }
--- Java_practice/SquareArtSquare.java
+++ Java_practice/SquareArtSquare.java
@@ -1,3 +1,5 @@
+package squareartkb;
+
 import javax.swing.*;
 import javax.swing.border.*;
 import javax.imageio.*;
@@ -7,26 +9,10 @@ import java.awt.image.*;
 import java.text.*;
 import java.io.*;
 
-public class SquareArtSquare extends JComponent{
-	public SquareArtSquare(){
-		setBorder(new LineBorder(Color.RED, 2));
-		setBackground(Color.YELLOW);
-		setBounds(0, 0, 150, 150);
-		setOpaque(false);
+public class SquareArtSquare extends JPanel{
+	public SquareArtSquare(String colour){
+		setBackground(Color.decode(colour));
+		setBounds(0, 0, 100, 100);
+		setOpaque(true);
 	 }
-	 
-	 public static void main(String[] args) {
-    JFrame sas = new JFrame("SquareArt Square");
-
- 
-    sas.setLayout(null);
-
-    SquareArtSquare blueSquare = new SquareArtSquare();
-    sas.add(blueSquare);
-
-    sas.setSize(200, 200);
-
-    sas.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    sas.setVisible(true);
-  }
 }
