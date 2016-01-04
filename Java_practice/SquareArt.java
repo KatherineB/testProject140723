4dad4601b4bd0ef010e12d162f0c8fe302996106
KatherineB <kbiefer@golden.net>
2016-01-04 15:20:37 -0500

    changed separate central area jpanels to an array of jpanels

--- Java_practice/SquareArt.java
+++ Java_practice/SquareArt.java
@@ -11,8 +11,17 @@ import java.text.*;
 import java.io.*;
 
 public class SquareArt extends JFrame{
+	 JPanel spot1 = new JPanel();
+	 JPanel spot2 = new JPanel();
+	 JPanel spot3 = new JPanel();
+	 JPanel spot4 = new JPanel();
+	 
+	 int NumSpots = 4;
+	 int i;
+	 JPanel[] spot = new JPanel[4];
+	 
      SquareArt(){
-	   setSize(600,400);	   
+	   setSize(250,200);	   
 	   setVisible(true);
 	   
 	   Container background = getContentPane();
@@ -23,42 +32,58 @@ public class SquareArt extends JFrame{
 	   centerArea.setLayout(new GridLayout(2,2));
 	   centerArea.setBackground(Color.decode("#bbaaff"));   
 	   background.add(centerArea, BorderLayout.CENTER);
+	   	 
+	   for( i = 0; i < NumSpots; i++){
+		   spot[i] = new JPanel(); 
+		   spot[i].setPreferredSize(new Dimension(30,30));
+		   spot[i].setBackground(Color.decode("#ffffff"));
+		   spot[i].setBorder(new LineBorder(Color.BLACK, 1));
+		   centerArea.add(spot[i]);
+	   }
 	   
 	   JPanel squaresAvail = new JPanel();
-	   squaresAvail.setLayout(new GridLayout(4,1));
-	   squaresAvail.setBackground(Color.decode("#55dd99"));
-	   squaresAvail.setPreferredSize(new Dimension(100,100));
+	   squaresAvail.setLayout(new GridLayout(8,1));
+	   squaresAvail.setBackground(Color.decode("#ffffff"));
+	//   squaresAvail.setPreferredSize(new Dimension(80,80));
 	   background.add(squaresAvail, BorderLayout.EAST);	   
 	   
 	   JPanel avail1 = new JPanel();
 	   avail1.setBackground(Color.decode("#ffffff"));
 	   SquareArtSquare greenSquare = new SquareArtSquare("#00dd00");
-	   greenSquare.setPreferredSize(new Dimension(80,80));	
+	   greenSquare.setPreferredSize(new Dimension(30,30));	
        avail1.add(greenSquare);	   
 	   squaresAvail.add(avail1);
 	   
 	   JPanel avail2 = new JPanel();
 	   avail2.setBackground(Color.decode("#ffffff"));
 	   SquareArtSquare blueSquare = new SquareArtSquare("#0000ff");
-	   blueSquare.setPreferredSize(new Dimension(80,80));
+	   blueSquare.setPreferredSize(new Dimension(30,30));
 	   avail2.add(blueSquare);
 	   squaresAvail.add(avail2);
 	   
 	   JPanel avail3 = new JPanel();
 	   avail3.setBackground(Color.decode("#ffffff"));
 	   SquareArtSquare yellowSquare = new SquareArtSquare("#ffff00");
-	   yellowSquare.setPreferredSize(new Dimension(80,80));
+	   yellowSquare.setPreferredSize(new Dimension(30,30));
 	   avail3.add(yellowSquare);
 	   squaresAvail.add(avail3);
 	   
 	   JPanel avail4 = new JPanel();
 	   avail4.setBackground(Color.decode("#ffffff"));
 	   SquareArtSquare redSquare = new SquareArtSquare("#ff0000");
-	   redSquare.setPreferredSize(new Dimension(80,80));
+	   redSquare.setPreferredSize(new Dimension(30,30));
 	   avail4.add(redSquare);
 	   squaresAvail.add(avail4);
+	   
+	   changeColour();
 	  
 	   setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 }
+	 private void changeColour(){		 
+		 spot[0].setBackground(Color.decode("#00ccaa"));
+		 spot[1].setBackground(Color.decode("#77ee33"));
+		 spot[2].setBackground(Color.decode("#dd6699"));
+		 spot[3].setBackground(Color.decode("#aa88ee"));
 	 }
 	 
      public static void main(String[]args){		 		  
