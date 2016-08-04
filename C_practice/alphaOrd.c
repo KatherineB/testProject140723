47a4b301af550e3cef57234d8c41c6d54d580800
KatherineB <kbiefer@golden.net>
2016-08-03 20:10:37 -0400

    added capability to compare positions of first two letters

--- C_practice/alphaOrd.c
+++ C_practice/alphaOrd.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int checkForDuplicates(int);
+void searchLetterSquareArray(char[]);
 int i,j,k;
 char checkArray[25] = "1234567890123456789012345";
 int counter = 0;
@@ -8,8 +10,8 @@ int flag = 0;
 char letterSquare[5][5];
 int outer = 5;
 int inner = 5;
-char guess;
-void searchLetterSquareArray(char);
+char sequence[];
+
 
 main(){
    char letters[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -40,10 +42,10 @@ main(){
         }   
        printf("\n");
     }
-   printf("Enter a letter\n");
-   scanf("%c", &guess);
+   printf("Enter a letter sequence\n");
+   scanf("%s", sequence);
    
-   searchLetterSquareArray(guess); 
+   searchLetterSquareArray(sequence); 
    return 0;
 } 
 
@@ -59,16 +61,43 @@ int checkForDuplicates(int pick){
       }
      return 0;
 }
-void searchLetterSquareArray(char guess){
+void searchLetterSquareArray(char sequence[]){
+    char letter1 = sequence[0];
+    char letter2 = sequence[1];
+    int x1, x2, y1, y2;
+    int xDiff, yDiff;
     
     for(i=0; i<outer; i++)
      {
       for(j=0; j<inner; j++)
         {
-          if(guess == letterSquare[i][j]){
-               printf("The letter %c is in position %d %d",guess,i,j);            
+          if(letter1 == letterSquare[i][j]){
+               printf("The first letter %c is in position %d %d \n",letter1,i,j);  
+               x1 = i;
+               y1 = j;          
           }
         }
+     }
+
+     for(i=0; i<outer; i++)
+     {
+      for(j=0; j<inner; j++)
+        {
+          if(letter2 == letterSquare[i][j]){
+               printf("The second letter %c is in position %d %d \n",letter2,i,j);   
+               x2 = i;
+               y2 = j;          
+          }
+        }
+     }
+     xDiff = x1 - x2;
+     yDiff = y1 - y2;
+
+     if( xDiff < 2  && yDiff < 2 && xDiff > -2 && yDiff > -2){
+         printf("Correct sequence!\n");
+     }
+     else{
+        printf("Not a correct sequence\n");
      }
 
 } 
