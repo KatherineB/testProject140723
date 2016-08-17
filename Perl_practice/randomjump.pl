5a2b434c9495c6cd12ab7d483f6477010fdf7a5b
KatherineB <kbiefer@golden.net>
2016-08-16 20:52:06 -0400

    improved arrays and spacing

--- Perl_practice/randomjump.pl
+++ Perl_practice/randomjump.pl
@@ -1,39 +1,45 @@
+#!/usr/bin/perl
 use strict;
 use warnings;
 
 my $x = 0;
 my $y = 0;
-my @row1 = (3,4,2,1);
-my @row2 = (4,4,2,3);
-my @row3 = (3,1,2,1);
-my @row4 = (2,2,3,1);
+
+my @square;
+ 
+$square[0][0] = 3;
+$square[0][1] = 4;
+$square[0][2] = 2;
+$square[0][3] = 4;
+$square[1][0] = 1;
+$square[1][1] = 4;
+$square[1][2] = 3;
+$square[1][3] = 1;
+$square[2][0] = 2;
+$square[2][1] = 4;
+$square[2][2] = 2;
+$square[2][3] = 3;
+$square[3][0] = 1;
+$square[3][1] = 4;
+$square[3][2] = 1;
+$square[3][3] = 3;
 
 
+for (my $i = 0; $i < 4; $i++){
+      for (my $j = 0; $j < 4; $j++){
+           print $square[$i][$j] , ' ';         
+      }
+      print "\n";
+}
 
 my @route = (1,2,3,3,1,2,3,2);
 
-for (my $i = 0; $i < 4; $i++){
-   print $row1[$i . ' '];
-}
-print "\n";
-for (my $i = 0; $i < 4; $i++){
-   print $row2[$i . ' '];
-}
-print "\n";
-for (my $i = 0; $i < 4; $i++){
-   print $row3[$i . ' '];
-}
-print "\n";
-for (my $i = 0; $i < 4; $i++){
-   print $row4[$i . ' '];
-}
-print "\n";
 
 for (my $i = 0; $i < 8; $i++){
-   print $route[$i . ' '];
+   print $route[$i] , ' ';
 }
 print "\n";
 
-print "Current co-ordinates:" . $x  . $y . "\n";
+print "Current co-ordinates:", ' ' , $x , ' ' , $y , "\n";
 
 
