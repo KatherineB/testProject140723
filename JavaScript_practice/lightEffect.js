2dee4fedf4916b21983ee9063aa7f63ad82b9e7c
KatherineB <kbiefer@golden.net>
2016-08-22 20:06:34 -0400

    added colour times array to run time only for selected colours

--- JavaScript_practice/lightEffect.js
+++ JavaScript_practice/lightEffect.js
@@ -1,5 +1,6 @@
 var coloursAvailable = new Array('#f03','#f74','#ff0','#0f0','#00f','#0fc','#a0f');
 var coloursPicked = new Array(0,0,0,0,0,0,0);
+var colourTimes = new Array(1,1,1,1,1,1,1);
 var NUMBER_OF_COLOURS = 7;
 var numberColoursPicked = 0;
 var firstColourPicked = -1;
@@ -14,27 +15,33 @@ function runShow(){
 
 function handleUserInput(){
 	 
+	 /* Gets length of time to display choosen colours */
+	 var intervalChosen = document.getElementsByName("time");
+	 
+	 for(var i = 0; i < intervalChosen.length; i++) {
+	   if(intervalChosen[i].checked == true) {
+		   interval = intervalChosen[i].value;
+	   }
+	 }
+	 alert("interval=" + interval);
 	 var colourArray = document.getElementsByName("colours");
 	
+	/* Gets colours chosen by user */
 	 for(var i = 0; i < colourArray.length; i++) {
 	   if(colourArray[i].checked == true) {
 		   //colourTest = colourArray[i].value;
-		   alert(colourArray[i].value);
+		  alert(colourArray[i].value);
 		  coloursPicked[i] = 1;
-		  numberColoursPicked++;
+	      colourTimes[i] = interval;
+		  numberColoursPicked++;		 
 	   }
+	    alert("colourTime=" + colourTimes[i]);
 	 }
-	 alert(numberColoursPicked);
 	 
-	 var intervalArray = document.getElementsByName("time");
 	 
-	 for(var i = 0; i < intervalArray.length; i++) {
-	  //  alert(intervalArray[i].value);
-	   if(intervalArray[i].checked == true) {
-		   interval = intervalArray[i].value;
-	   }
-	 }
+	 
 	
+	 /* Gets number of times to go through all the colours */
 	 var loopArray = document.getElementsByName("loop");
 	 
 	 for(var i = 0; i < loopArray.length; i++) {
@@ -116,13 +123,13 @@ function runCycle(){
 								if(coloursPicked[6]){
 								   displayColour(coloursAvailable[6]);
 								}
-							}, interval);
-						}, interval);
-					}, interval);
-				}, interval);
-			},interval);
-		},interval);
-	}, interval);
+							}, colourTimes[6]);
+						}, colourTimes[5]);
+					}, colourTimes[4]);
+				}, colourTimes[3]);
+			},colourTimes[2]);
+		},colourTimes[1]);
+	}, colourTimes[0]);
 }
 function displayColour(colour){
 	 document.getElementById('mainDiv').style.backgroundColor = colour;	 
