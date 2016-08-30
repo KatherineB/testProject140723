bd6651bdf4eab8ab5c09afcc2390c88776e486f3
KatherineB <kbiefer@golden.net>
2016-08-29 20:55:56 -0400

    got the selection of the number of loops working

--- JavaScript_practice/lightEffect.js
+++ JavaScript_practice/lightEffect.js
@@ -6,11 +6,12 @@ var numberColoursPicked = 0;
 var firstColourPicked = -1;
 var interval;
 var loop;
+var numberLoops;
 
 function runShow(){
 	handleUserInput();
 	controlNumberLoops();
-	runCycle();
+	//runCycle();
 }
 
 function handleUserInput(){
@@ -23,24 +24,21 @@ function handleUserInput(){
 		   interval = intervalChosen[i].value;
 	   }
 	 }
-	 alert("interval=" + interval);
+	// alert("interval=" + interval);
 	 var colourArray = document.getElementsByName("colours");
 	
 	/* Gets colours chosen by user */
 	 for(var i = 0; i < colourArray.length; i++) {
 	   if(colourArray[i].checked == true) {
 		   //colourTest = colourArray[i].value;
-		  alert(colourArray[i].value);
+		 // alert(colourArray[i].value);
 		  coloursPicked[i] = 1;
 	      colourTimes[i] = interval;
 		  numberColoursPicked++;		 
 	   }
-	    alert("colourTime=" + colourTimes[i]);
+	   // alert("colourTime=" + colourTimes[i]);
 	 }
-	 
-	 
-	 
-	
+	 	
 	 /* Gets number of times to go through all the colours */
 	 var loopArray = document.getElementsByName("loop");
 	 
@@ -48,28 +46,34 @@ function handleUserInput(){
 	   if(loopArray[i].checked == true) {
 		   numberLoops = loopArray[i].value;
 	   }
-	 }
-	  
+	 }	  
 	// loop = interval * NUMBER_OF_COLOURS;
 }
 
 function controlNumberLoops(){	 
-    // alert(numberLoops);
-	loop = 1000;
+   //  alert(numberLoops);
+	loop = 10000 * numberColoursPicked;
+/*	for(var i=0; i<numberLoops; i++){
+		runCycle();
+	}*/
 	 setTimeout(function() {
 			if(numberLoops > 0){
-			   runCycle();
+				//alert("numberLoops > 0");
+			   runCycle();			   
 			}
 			setTimeout(function() {
 				if(numberLoops > 1){
+					//alert("numberLoops > 1");
 				   runCycle();
 				}
 				setTimeout(function() {
 					if(numberLoops > 2){
+						//alert("numberLoops > 2");
 					   runCycle();
 					}
 					setTimeout(function() {
 						if(numberLoops > 3){
+							//alert("numberLoops > 3");
 						   runCycle();
 						}
 						setTimeout(function() {
@@ -90,37 +94,63 @@ function controlNumberLoops(){
 				    }, loop);
 				}, loop);
 			}, loop);
-		}, 1000);
+		}, 1000); 
 }
 
 function runCycle(){
-    alert("runC");
+   // alert("runC");
+	//alert(colourTimes[0]);
+	/*setTimeout(function() {
+		if(coloursPicked[0]){
+			//alert("in 0");
+		   displayColour(coloursAvailable[0]);
+		}
+	}, colourTimes[0]);
+	setTimeout(function() {
+		if(coloursPicked[1]){
+			//alert("in 1");
+		   displayColour(coloursAvailable[1]);
+		}
+	}, colourTimes[1]);
+	setTimeout(function() {
+		if(coloursPicked[2]){
+			//alert("in 2");
+		   displayColour(coloursAvailable[2]);
+		}
+	}, colourTimes[2]);*/
 	setTimeout(function() {
 	    if(coloursPicked[0]){
+			//alert("in 0");
 			displayColour(coloursAvailable[0]);
 		}
 		setTimeout(function() {
 		    if(coloursPicked[1]){
+			//	alert("in 1");
 				displayColour(coloursAvailable[1]);
 		    }
 			setTimeout(function() {
 			    if(coloursPicked[2]){
+				//	alert("in 2");
 				  displayColour(coloursAvailable[2]);
 				}
 				setTimeout(function() {
 					if(coloursPicked[3]){
+						//alert("in 3");
 					  displayColour(coloursAvailable[3]);
 					}
 					setTimeout(function() {
 						if(coloursPicked[4]){
+						//	alert("in 4");
 						   displayColour(coloursAvailable[4]);
 						}
 						setTimeout(function() {
 							if(coloursPicked[5]){
+							//	alert("in 5");
 							   displayColour(coloursAvailable[5]);
 							}
 							setTimeout(function() {
 								if(coloursPicked[6]){
+									//alert("in 6");
 								   displayColour(coloursAvailable[6]);
 								}
 							}, colourTimes[6]);
