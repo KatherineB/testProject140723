f267a4dad72686d9ce36fa95ef6491c1ad66e64c
KatherineB <kbiefer@golden.net>
2016-09-28 22:14:05 -0400

    added arrays for guesses and results

--- Python_practice/sequence.py
+++ Python_practice/sequence.py
@@ -1,48 +1,43 @@
-#x = float(random.random())
-#x = random.random()
-#x = float(347.01/3.9)
-#print(x)
-ts_1 = 2
-ts_2 = 3
-ts_3 = 1
-ts_4 = 1
-ts_5 = 3
-
-c_1 = 0
-c_2 = 0
-c_3 = 0
-c_4 = 0
-c_5 = 0
-
+import random
+from array import *
+randomNumbers = array('i',[0,0,0,0,0])
+guesses = array('i',[0,0,0,0,0])
+checks = array('i',[0,0,0,0,0])
 correct = 0
+for i in randomNumbers:
+    randomNumbers[i] = random.randint(1,3)
+    print(randomNumbers[i])
 
-g_1 = int(input("Enter Number 1:"))
-g_2 = int(input("Enter Number 2:"))
-g_3 = int(input("Enter Number 3:"))
-g_4 = int(input("Enter Number 4:"))
-g_5 = int(input("Enter Number 5:"))
+for i in randomNumbers:
+     print(randomNumbers.index(i))
 
-if g_1 == ts_1:
-      if g_2 == ts_2:
-           c_1 = 1
+for k in guesses:
+    guesses[k] = int(input("Enter Number:"))
 
-if g_2 == ts_2:
-      if g_1 == ts_1 and g_3 == ts_3:
-           c_2 = 1
 
-if g_3 == ts_3:
-      if g_2 == ts_2 and g_4 == ts_4:
-           c_3 = 1
+if guesses[0] == randomNumbers[0]:
+      if guesses[1] == randomNumbers[1]:
+           checks[0] = 1
 
-if g_4 == ts_4:
-      if g_3 == ts_3 and g_5 == ts_5:
-           c_4 = 1
+if guesses[1] == randomNumbers[1]:
+      if guesses[0] == randomNumbers[0] and guesses[2] == randomNumbers[2]:
+           checks[1] = 1
 
-if g_5 == ts_5:
-      if g_4 == ts_4:
-           c_5 = 1
+if guesses[2] == randomNumbers[2]:
+      if guesses[1] == randomNumbers[1] and guesses[3] == randomNumbers[3]:
+           checks[2] = 1
 
-correct = c_1 + c_2 + c_3 + c_4 + c_5
+if guesses[3] == randomNumbers[3]:
+      if guesses[2] == randomNumbers[2] and guesses[4] == randomNumbers[4]:
+           checks[3] = 1
+
+if guesses[4] == randomNumbers[4]:
+      if guesses[3] == randomNumbers[3]:
+           checks[4] = 1
+
+
+
+correct = checks[0] + checks[1] + checks[2] + checks[3] + checks[4]
 
 
 print(correct)
