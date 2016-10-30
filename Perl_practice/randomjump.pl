f932cba25803b55660bb6adb7183cbb88b32db0c
KatherineB <kbiefer@golden.net>
2016-10-30 19:56:54 -0400

    put in temp fix for input problem and then got everything else about instructions display working

--- Perl_practice/randomjump.pl
+++ Perl_practice/randomjump.pl
@@ -34,13 +34,15 @@ for (my $i = 0; $i < 8; $i++){
 
 print "\n";
 print "Welcome to Random Jump\n";
-print "Do you want instructions on how to play? (y or n): ";
+#print "Do you want instructions on how to play? (y or n): ";
+print "Do you want instructions on how to play? (Enter 1 for Yes, 0 for No.): ";
 $info = <STDIN>;
 
-if ($info == "y" || $info == "Y"){
-       showHowToPlay();
+#if ($info == "y" || $info == "Y"){               
+if ($info == 1){    # Temporary fix until problem is solved
+       showHowToPlay();     
 }
-
+print "\n";
 
 while ($k < $routeLength){
 	for (my $i = 0; $i < 4; $i++){
@@ -50,6 +52,8 @@ while ($k < $routeLength){
 	      print "\n";
 	}
 	print "\n";
+
+        print "Jump Sequence:\n";
 
 	for (my $i = 0; $i < $routeLength; $i++){
 	   print $route[$i] , ' ';
@@ -84,7 +88,7 @@ while ($k < $routeLength){
         if ($k < $routeLength - 1){
           $nextStep = $route[$k+1];
         }
-       print "step=", $step, "\n";
+   #    print "step=", $step, "\n";
 
         $legalMove = validateMove($x,$y,$x_prev,$y_prev,$step);
         
@@ -99,11 +103,11 @@ while ($k < $routeLength){
                 $k++;
                 
                 if ($k < $routeLength){
-		       print "Current Score=", $score, "\n\n";
+		       print "Current Score = ", $score, "\n\n";
                 }
         }
         else{
-                print "Please enter a legal move.\n\n";
+                print "Please try again.\n\n";
         }
 
         # It is only possible for there to be no legal moves available when the player must
@@ -182,33 +186,56 @@ sub checkForLegalMoves {
 
        if ( ($x + 3) > -1 && ($x + 3) < 4 ){
              $option = 1;
-              print "1 legal options found\n";
        }
        elsif ( ($x - 3) > -1 && ($x - 3) < 4 ){
              $option = 1;
-             print "2 legal options found\n";
        }
        elsif ( ($y + 3) > -1 && ($y + 3) < 4 ){
              $option = 1;
-             print "3 legal options found\n";
        }
        elsif ( ($y - 3) > -1 && ($y - 3) < 4 ){
              $option = 1;
-             print "3 legal options found\n";
        }
        else{
-             print "No legal options found\n";
+             $option = 0;
        }
        return($option);
 }
 
 sub showHowToPlay {
+       my $blank;
+       print "\n"; 
        print "How to Play Random Jump\n";
        print "\n";   
-       print "In Random Jump, the player is confronted with a 4 X 4 square of numbers\n"
-       print "from 1 to 4 and a sequence of 8 numbers that contains numbers from 1 to 3.\"
-
-
+       print "In Random Jump, the player is confronted with a 4 X 4 square of numbers from\n";
+       print "1 to 4 and a jump sequence of 8 numbers that contains numbers from 1 to 3.\n";
+       print "\n";
+       print "At the start of the game, the player is considered to be in the upper left\n";
+       print "corner, co-ordinates 0,0.  The player must jump around the square in different\n";
+       print "directions, always moving the number of spaces dictated by the sequence of 8\n";
+       print "numbers.  For example, starting at 0,0, if the first number in the sequence is\n";
+       print "3, one legal move would be 0,3, a move of 3 to the right.  A move of only 2 to\n";
+       print "the right would not be allowed in this example.  The player makes 8 moves\n";
+       print "according to the 8 numbers in the jump sequence and then the game ends.\n";
+       print "Diagonal moves are not allowed.\n";
+       print "\n";
+       print "There is no particular destination that the player must get to.  Instead, the\n";
+       print "goal is to accumulate points by landing on numbers.  What ever number the player\n";
+       print "lands on, the player gets to score that number of points.  However, the points\n";
+       print "are only there for the first landing.  After that, the spot is only zero.  It is\n";
+       print "legal to land on a zero but it earns no points.\n";
+       print "\n";
+       print "Players should try to plan their routes so that they land on the highest\n";
+       print "numbers.  There is also another reason for careful route planning.  It\n";
+       print "is possible to get stuck in a position where no legal moves are possible.  If \n";
+       print "this happens, the game ends early and the player gets stuck with a lower score.\n";
+       print "It is only possible to get stuck when a move of 3 must be made.  For 1 or 2,\n";
+       print "there is always enough space to move somewhere without going outside the square.\n";
+       print "Thus, players must watch for threes in the jump sequence and plan a route that\n";
+       print "avoids this hazard.\n";
+       print "\n\n";
+       print "Hit enter when ready to continue with game.";
+       $blank = <STDIN>;
        return;
 }
 
