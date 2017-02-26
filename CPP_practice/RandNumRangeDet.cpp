49e8f158ddfce15656444a0669979d2bcd41c12a
KatherineB <kbiefer@golden.net>
2017-02-25 20:43:00 -0500

    more work on random numbers and some work on range determining

--- CPP_practice/RandNumRangeDet.cpp
+++ CPP_practice/RandNumRangeDet.cpp
@@ -6,29 +6,33 @@ using std::endl;
 #include <cstdlib>
 using std::rand;
 
-
+#include "time.h"
 
 int main(int argc,int argv[])
 {
 	int i;
     int j;
+	int k;
 	int loop;
 	int done = 0;
 	int shelf;
 	int numbers[10][10];
 	char blank;
+	int low[10] = {2,8,16,24,39,42,58,61,78,81};
+	int high[10] = {7,14,21,34,47,58,71,73,85,97};
+	int range[10];
+	
 
    // srand(384);
-	srand(1000);
+	srand(time(0));
+	
 	for (i=0; i<10; i++){	
 			for (j=0; j<10; j++){
 				numbers[i][j] = rand()%100;
-				//cout << numbers[0][j] << endl;
 			}			
 	}
 	cout << endl;
-	for (i=0; i<3; i++){
-		    
+	for (i=0; i<10; i++){	    
 			do{
 				done = 1;
 				for (j=0; j<9; j++){
@@ -38,7 +42,6 @@ int main(int argc,int argv[])
 						numbers[i][j+1] = shelf;
 						done = 0;
 					}
-				//	cout << "done=" << done << "  ";
 				}
 				cout << endl;
 
@@ -46,11 +49,20 @@ int main(int argc,int argv[])
 		    
 	}
 	
-	for (i=0; i<3; i++){
+	/*for (i=0; i<10; i++){
 		for (j=0; j<10; j++){
-			cout << numbers[0][j] << endl;
+			//cout << numbers[i][0] << endl;
 		} 
-		cout << "----" << endl;			
+		//cout << "----" << endl;			
+	}*/
+
+	/*for (i=0;i<10; i++){
+		cout << numbers[i][0] << endl;
+	}*/
+
+	for (k=0; k<10; k++){
+		range[k] = high[k] - low[k];	
+		cout << range[k] << endl;
 	}
 	
 	cin >> blank ;
