872afd009e0fef858411e232a5f3cbb3232626d5
KatherineB <kbiefer@golden.net>
2017-03-02 20:32:49 -0500

    worked on finding the highest and lowest numbers in the arrays

--- CPP_practice/RandNumRangeDet.cpp
+++ CPP_practice/RandNumRangeDet.cpp
@@ -4,9 +4,9 @@ using std::cin;
 using std::endl;
 
 #include <cstdlib>
-using std::rand;
+using namespace std;
 
-#include "time.h"
+#include <ctime>
 
 int main(int argc,int argv[])
 {
@@ -18,8 +18,11 @@ int main(int argc,int argv[])
 	int shelf;
 	int numbers[10][10];
 	char blank;
-	int low[10] = {2,8,16,24,39,42,58,61,78,81};
-	int high[10] = {7,14,21,34,47,58,71,73,85,97};
+	int low;
+	int high;
+	int section[10];
+	//int low[10] = {2,8,16,24,39,42,58,61,78,81};
+	//int high[10] = {7,14,21,34,47,58,71,73,85,97};
 	int range[10];
 	
 
@@ -49,21 +52,38 @@ int main(int argc,int argv[])
 		    
 	}
 	
-	/*for (i=0; i<10; i++){
+/*	for (i=0; i<10; i++){
 		for (j=0; j<10; j++){
-			//cout << numbers[i][0] << endl;
+			cout << numbers[i][j] << endl;
 		} 
-		//cout << "----" << endl;			
-	}*/
+		cout << "----" << endl;			
+	} */
 
-	/*for (i=0;i<10; i++){
+	for (i=0;i<10; i++){
+		
 		cout << numbers[i][0] << endl;
-	}*/
+		section[i] = numbers[i][0];
+        low = section[i];
+		high = section[i];
 
-	for (k=0; k<10; k++){
+		for (k=0; k<10; k++){
+			if (section[k] < low){
+				low = section[k];
+			}
+			if (section[k] > high){
+				high = section[k];
+			}
+		}
+
+	}
+
+	cout << "low=" << low << endl;
+	cout << "high=" << high << endl;
+
+/*	for (k=0; k<10; k++){
 		range[k] = high[k] - low[k];	
 		cout << range[k] << endl;
-	}
+	}*/
 	
 	cin >> blank ;
 
