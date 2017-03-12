6dcba0237b22ff793595c76aa26d4e297e7877e3
KatherineB <kbiefer@golden.net>
2017-03-11 20:45:19 -0500

    added low, high, range and average calculations and their output

--- CPP_practice/RandNumRangeDet.cpp
+++ CPP_practice/RandNumRangeDet.cpp
@@ -13,28 +13,29 @@ int main(int argc,int argv[])
 	int i;
     int j;
 	int k;
-	int loop;
+	int LIMIT = 70;
 	int done = 0;
 	int shelf;
 	int numbers[10][10];
 	char blank;
-	int low;
-	int high;
+	int lowest;
+	int highest;
 	int section[10];
+	int low[10];
+	int high[10];
+	int range[10];
+	double average[10];
 	//int low[10] = {2,8,16,24,39,42,58,61,78,81};
 	//int high[10] = {7,14,21,34,47,58,71,73,85,97};
-	int range[10];
 	
-
-   // srand(384);
 	srand(time(0));
 	
 	for (i=0; i<10; i++){	
 			for (j=0; j<10; j++){
-				numbers[i][j] = rand()%100;
+				numbers[i][j] = (rand()%LIMIT) + 1;
 			}			
 	}
-	cout << endl;
+
 	for (i=0; i<10; i++){	    
 			do{
 				done = 1;
@@ -46,41 +47,33 @@ int main(int argc,int argv[])
 						done = 0;
 					}
 				}
-				cout << endl;
-
-			}while(done == 0);
-		    
+			}while(done == 0);	    
 	}
 	
-/*	for (i=0; i<10; i++){
-		for (j=0; j<10; j++){
-			cout << numbers[i][j] << endl;
-		} 
-		cout << "----" << endl;			
-	} */
+	for(j=0;j<10;j++){
+			for (i=0;i<10; i++){				
+				section[i] = numbers[i][j];
+				lowest = section[i];
+				highest = section[i];
 
-	for (i=0;i<10; i++){
-		
-		cout << numbers[i][0] << endl;
-		section[i] = numbers[i][0];
-        low = section[i];
-		high = section[i];
-
-		for (k=0; k<10; k++){
-			if (section[k] < low){
-				low = section[k];
+				for (k=0; k<10; k++){
+					if (section[k] < lowest){
+						lowest = section[k];
+					}
+					if (section[k] > highest){
+						highest = section[k];
+					}
+				}
 			}
-			if (section[k] > high){
-				high = section[k];
-			}
-		}
-
+			low[j] = lowest;
+			high[j] = highest;
+			range[j] = highest - lowest;
+			average[j] = (lowest + highest)/2.0;       
 	}
-
-	cout << "low=" << low << endl;
-	cout << "high=" << high << endl;
-
-/*	for (k=0; k<10; k++){
+    for (k=0; k<10; k++){
+		cout << "low=" << low[k] << "  high=" << high[k] << "  range=" << range[k] << "  average=" << average[k] << endl;
+	}
+	/*for (k=0; k<10; k++){
 		range[k] = high[k] - low[k];	
 		cout << range[k] << endl;
 	}*/
