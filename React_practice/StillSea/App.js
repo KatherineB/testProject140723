cb4cccc82d142f77bb71410e2cda145b865a3cb8
KatherineB <kbiefer@golden.net>
2017-12-06 15:14:40 -0500

    fixed logo image problem, worked on radio button controlled slide show

--- React_practice/StillSea/App.js
+++ React_practice/StillSea/App.js
@@ -13,15 +13,38 @@ class App extends Component {
     return (
       <div className="App">
 			<div className="App-header">
-				  <div className="logo-box">
-					   <img className="logo" src="logo.png"/>
+				  <div className="logo-box">	  
+					   <img className="logo" src={ require('./images/logo.png') } />
 				  </div>
 				  <div className="main-title">
 					   Still Sea
 				  </div>
 				  <div style={{clear:'both'}}></div>
 			</div>    
+			
             <div className="section-1">
+			      <div className="text-slide" id="ts01">
+				       A stitch in time saves nine.
+				  </div>
+				  <div className="text-slide" id="ts02">
+				       Better late than never.
+				  </div>
+				  <div className="text-slide" id="ts03">
+				       An ounce of prevention is worth a pound of cure.
+				  </div>
+				  <div style={{clear:'both'}}></div>
+				  <div>
+				       <div className="radbtnA-holder">
+							  <div className="radbtnA" onClick={() => this.radbtnA_1()}></div>
+					   </div>
+					   <div className="radbtnA-holder">
+							  <div className="radbtnA" onClick={() => this.radbtnA_2()}></div>
+					   </div>
+					   <div className="radbtnA-holder">
+							  <div className="radbtnA" onClick={() => this.radbtnA_3()}></div>
+					   </div>
+					  <div style={{clear:'both'}}></div>
+				  </div>
             </div>			
       </div>
     );
