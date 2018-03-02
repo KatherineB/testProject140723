6485228916842e35de6ba3dfad0eff2c6a139cbb
KatherineB <kbiefer@golden.net>
2018-03-01 19:16:44 -0500

    first commit, new files just being started

---
+++ Python_practice/fileDex_search.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# FileDex_search.py
+# -for searching
+
+#inputFile = open("datafile.txt","r")
+with open("datafile.txt","r") as inputFile:
+    for line in inputFile:
+        print line
---
+++ Python_practice/fileDex_sort.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# FileDex_sort.py
+# -for sorting
+
+#inputFile = open("newfile.txt","r")
+with open("newfile.txt","r") as inputFile:
+    for line in inputFile:
+        print line
+
