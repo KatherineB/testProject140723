cf193b3a91c40d4db752a1ea3047a3e4f1d76d91
KatherineB <kbiefer@golden.net>
2018-09-13 17:15:00 -0400

    added slider switches for initial state and option to display extra info

--- JavaScript_practice/Hadamard_1Q.js
+++ JavaScript_practice/Hadamard_1Q.js
@@ -1,8 +1,24 @@
-let firstState = -1;
+let iState = -1;
 let hadsDone = 0;
-let arrows = 1;
-function setInitialState(iState){
-    firstState = iState;
+let arrows = 0;
+let firstDone = 0;
+
+setInitialState(0);
+
+function setInitialState(firstDone){
+
+	if(firstDone){
+		if(document.getElementById('init-slider-state').checked){
+		  iState = 1;
+		}
+		else{
+		   iState = 0;
+		}
+	}
+	else{
+		iState = 0;
+		firstDone = 1;
+	}
     if(iState == 0){
 	    document.getElementById('tf-00').style.backgroundColor = "#5ea4ee";
 		document.getElementById('bf-00').style.backgroundColor = "#5ea4ee";
@@ -20,78 +36,80 @@ function setInitialState(iState){
 		document.getElementById('bf-00').innerHTML = "";
 	}
 }
+function setExtraState(){
+	if(document.getElementById('extra-slider-state').checked){
+	  arrows = 1;
+	}
+	else{
+	   arrows = 0;
+	}
+}
 function doHadamard(){
-       if(firstState > -1){
-			if(hadsDone == 0){
-			   if(firstState == 0){
-				document.getElementById('tf-01').style.backgroundColor = "#ccbb88";
-				document.getElementById('bf-01').style.backgroundColor = "#5ea4ee";
-				document.getElementById('tf-11').style.backgroundColor = "#ccbb88";
-				document.getElementById('bf-11').style.backgroundColor = "#5ea4ee";
-				document.getElementById('bf-01').innerHTML = "0.5 E";
-				document.getElementById('bf-11').innerHTML = "0.5 E";
-                                if(arrows == 1){
-                                    document.getElementById('svg-t0-s1').style.display = 'block';
-                                }
-			   }
-			   else if(firstState == 1){
-				document.getElementById('tf-01').style.backgroundColor = "#ccbb88";
-				document.getElementById('bf-01').style.backgroundColor = "#5ea4ee";
-				document.getElementById('tf-11').style.backgroundColor = "#ccbb88";
-				document.getElementById('bf-11').style.backgroundColor = "#5ea4ee";
-				document.getElementById('bf-01').innerHTML = "0.5 E";
-				document.getElementById('bf-11').innerHTML = "0.5 W";
-                                if(arrows == 1){
-                                    document.getElementById('svg-t1-s1').style.display = 'block';
-                                }
-			   }
-			   hadsDone++;
-			}
-			else if(hadsDone == 1){
-			   if(firstState == 0){
-				document.getElementById('tf-02').style.backgroundColor = "#5ea4ee";
-				document.getElementById('bf-02').style.backgroundColor = "#5ea4ee";
-				document.getElementById('tf-12').style.backgroundColor = "#ccbb88";
-				document.getElementById('bf-12').style.backgroundColor = "#ccbb88";
-				document.getElementById('bf-02').innerHTML = "E";
-				document.getElementById('bf-12').innerHTML = "";
-                                if(arrows == 1){
-                                     document.getElementById('svg-t0-s2').style.display = 'block';
-                                     document.getElementById('ps-holder').style.display = 'inline-block';
-                                     document.getElementById('ps-holder').style.marginTop = '-19em';
-                                     document.getElementById('small-label-b').innerHTML = '0.25 E';
-                                     document.getElementById('small-label-d').innerHTML = '0.25 W';
-                                }
-			   }
-			   else if(firstState == 1){
-				document.getElementById('tf-02').style.backgroundColor = "#ccbb88";
-				document.getElementById('bf-02').style.backgroundColor = "#ccbb88";
-				document.getElementById('tf-12').style.backgroundColor = "#5ea4ee";
-				document.getElementById('bf-12').style.backgroundColor = "#5ea4ee";
-				document.getElementById('bf-02').innerHTML = "";
-				document.getElementById('bf-12').innerHTML = "E";
-                                if(arrows == 1){
-                                     document.getElementById('svg-t1-s2').style.display = 'block';
-                                     document.getElementById('ps-holder').style.display = 'inline-block';
-                                     document.getElementById('ps-holder').style.marginTop = '-3.7em';
-                                     document.getElementById('small-label-b').innerHTML = '0.25 W';
-                                     document.getElementById('small-label-d').innerHTML = '0.25 E';
-                                }
-			   }
-			   hadsDone++;
-			}
-	        else{
-			   alert("Only two Hadamards can be performed for this demonstration. Please click the Reset button if you want to try again.");
+	if(hadsDone == 0){
+	   if(iState == 0){
+		document.getElementById('tf-01').style.backgroundColor = "#ccbb88";
+		document.getElementById('bf-01').style.backgroundColor = "#5ea4ee";
+		document.getElementById('tf-11').style.backgroundColor = "#ccbb88";
+		document.getElementById('bf-11').style.backgroundColor = "#5ea4ee";
+		document.getElementById('bf-01').innerHTML = "0.5 E";
+		document.getElementById('bf-11').innerHTML = "0.5 E";
+			if(arrows == 1){
+				document.getElementById('svg-t0-s1').style.display = 'block';
 			}
 	   }
-	   else{
-	       alert('Please choose the initial state by clicking on the zero button or the one button');
+	   else if(iState == 1){
+		document.getElementById('tf-01').style.backgroundColor = "#ccbb88";
+		document.getElementById('bf-01').style.backgroundColor = "#5ea4ee";
+		document.getElementById('tf-11').style.backgroundColor = "#ccbb88";
+		document.getElementById('bf-11').style.backgroundColor = "#5ea4ee";
+		document.getElementById('bf-01').innerHTML = "0.5 E";
+		document.getElementById('bf-11').innerHTML = "0.5 W";
+			if(arrows == 1){
+				document.getElementById('svg-t1-s1').style.display = 'block';
+			}
 	   }
+	   hadsDone++;
+	}
+	else if(hadsDone == 1){
+	   if(iState == 0){
+		document.getElementById('tf-02').style.backgroundColor = "#5ea4ee";
+		document.getElementById('bf-02').style.backgroundColor = "#5ea4ee";
+		document.getElementById('tf-12').style.backgroundColor = "#ccbb88";
+		document.getElementById('bf-12').style.backgroundColor = "#ccbb88";
+		document.getElementById('bf-02').innerHTML = "E";
+		document.getElementById('bf-12').innerHTML = "";
+			if(arrows == 1){
+				 document.getElementById('svg-t0-s2').style.display = 'block';
+				 document.getElementById('ps-holder').style.display = 'inline-block';
+				 document.getElementById('ps-holder').style.marginTop = '-19em';
+				 document.getElementById('small-label-b').innerHTML = '0.25 E';
+				 document.getElementById('small-label-d').innerHTML = '0.25 W';
+			}
+	   }
+	   else if(iState == 1){
+		document.getElementById('tf-02').style.backgroundColor = "#ccbb88";
+		document.getElementById('bf-02').style.backgroundColor = "#ccbb88";
+		document.getElementById('tf-12').style.backgroundColor = "#5ea4ee";
+		document.getElementById('bf-12').style.backgroundColor = "#5ea4ee";
+		document.getElementById('bf-02').innerHTML = "";
+		document.getElementById('bf-12').innerHTML = "E";
+			if(arrows == 1){
+				 document.getElementById('svg-t1-s2').style.display = 'block';
+				 document.getElementById('ps-holder').style.display = 'inline-block';
+				 document.getElementById('ps-holder').style.marginTop = '-3.7em';
+				 document.getElementById('small-label-b').innerHTML = '0.25 W';
+				 document.getElementById('small-label-d').innerHTML = '0.25 E';
+			}
+	   }
+	   hadsDone++;
+	}
+	else{
+	   alert("Only two Hadamards can be performed for this demonstration. Please click the Reset button if you want to try again.");
+	}
 } 
 function reset(){
-    firstState = -1;
     hadsDone = 0;
-	
+
 	document.getElementById('tf-00').style.backgroundColor = "#ccbb88";
 	document.getElementById('bf-00').style.backgroundColor = "#ccbb88";
 	document.getElementById('tf-10').style.backgroundColor = "#ccbb88";
@@ -111,10 +129,12 @@ function reset(){
 	document.getElementById('bf-10').innerHTML = "";
     document.getElementById('bf-11').innerHTML = "";
     document.getElementById('bf-12').innerHTML = "";
-      document.getElementById('svg-t0-s1').style.display = 'none';
-      document.getElementById('svg-t1-s1').style.display = 'none';
-      document.getElementById('svg-t0-s2').style.display = 'none';
-      document.getElementById('svg-t1-s2').style.display = 'none';
-      document.getElementById('ps-holder').style.display = 'none';
-}
+	document.getElementById('svg-t0-s1').style.display = 'none';
+	document.getElementById('svg-t1-s1').style.display = 'none';
+	document.getElementById('svg-t0-s2').style.display = 'none';
+	document.getElementById('svg-t1-s2').style.display = 'none';
+	document.getElementById('ps-holder').style.display = 'none';
+
+	setInitialState(1);
+	}
 
