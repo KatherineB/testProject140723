9dcf0abe19294bc280023cfa075956c6d7ee1ad9
KatherineB <kbiefer@golden.net>
2018-10-03 17:42:30 -0400

    added next section and started work on loading JSON file

---
+++ React_practice/StillSea/StillSea_JSON_load.js
@@ -0,0 +1,45 @@
+﻿import React, { Component } from "react";
+
+class StillSea_JSON_load extends Component {
+	constructor() {
+    super();
+    
+    this.state = { 
+	       
+		};  
+       
+/*	   	fetch('data/planets.json')
+  .then(function(response) {
+    console.log(`status: ${response.status}`);
+    console.dir(response.body);
+    return response.json() 
+  })*/
+ /* .then(function(myJson) {
+    document.write(
+      `User: ${myJson.name} <br/>
+      Email: ${myJson.email} <br />
+      Website: ${myJson.website} <br/>
+      City: ${myJson.address.city} `
+    );
+    return myJson
+  })
+  .then(console.log)*/
+	   
+	   
+	}
+
+	
+
+  
+  render() {
+    return (
+        <div className="section-3">
+		    <div className="section-title">Planets</div>
+			
+			  
+        </div>		
+    );
+  }
+}
+
+export default StillSea_JSON_load;
--- React_practice/StillSea/StillSea_Main.js
+++ React_practice/StillSea/StillSea_Main.js
@@ -7,6 +7,7 @@
 import StillSea_Home from "./StillSea_Home";
 import StillSea_ImgRotate from "./StillSea_ImgRotate";
 import StillSea_ImgOverlay from "./StillSea_ImgOverlay";
+import StillSea_JSON_load from "./StillSea_JSON_load";
 
 class StillSea_Main extends Component {	
 	
@@ -16,7 +17,8 @@ class StillSea_Main extends Component {
     this.state = { 
 		   active_link_0: {color: '#fff'},
 		   active_link_1: {color: '#fff'},
-		   active_link_2: {color: '#fff'}		  
+		   active_link_2: {color: '#fff'},	
+           active_link_3: {color: '#fff'}		   
 		};      
 	}
 	
@@ -24,7 +26,8 @@ class StillSea_Main extends Component {
 	  this.setState({
 		    active_link_0: {color: '#fff'},
 			active_link_1: {color: '#fff'},
-			active_link_2: {color: '#fff'}
+			active_link_2: {color: '#fff'},
+			active_link_3: {color: '#fff'}
 	   });
 	   
 	   switch(section){
@@ -36,6 +39,9 @@ class StillSea_Main extends Component {
 			  break;
 		   case 2: 
 	          this.setState({ active_link_2: {color: '#0f0'} });
+			  break;
+		   case 3: 
+	          this.setState({ active_link_3: {color: '#0f0'} });
 			  break;
 		   default:
 		      alert("Nothing matched in switch");
@@ -59,12 +65,14 @@ class StillSea_Main extends Component {
             <div className="menu-item"  onClick={() => this.setActiveLink(0)} ><NavLink exact to="/" style={this.state.active_link_0}>Home</NavLink></div>
             <div className="menu-item" onClick={() => this.setActiveLink(1)}><NavLink to="/StillSea_ImgRotate" style={this.state.active_link_1}>Section 1</NavLink></div>
             <div className="menu-item" onClick={() => this.setActiveLink(2)}><NavLink to="/StillSea_ImgOverlay" style={this.state.active_link_2}>Section 2</NavLink></div>
+			 <div className="menu-item" onClick={() => this.setActiveLink(3)}><NavLink to="/StillSea_JSON_load" style={this.state.active_link_3}>Section 3</NavLink></div>
 			<div style={{clear:'both'}}></div>
           </div>
           <div className="content">
             <Route exact path="/" component={StillSea_Home}/>
             <Route path="/StillSea_ImgRotate" component={StillSea_ImgRotate}/>
             <Route path="/StillSea_ImgOverlay" component={StillSea_ImgOverlay}/>
+			<Route path="/StillSea_JSON_load" component={StillSea_JSON_load}/>
           </div>
         </div>
       </HashRouter>
