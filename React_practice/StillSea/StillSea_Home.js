ffe93c065f633da4cb34e652bdc4a9045de643b7
KatherineB <kbiefer@golden.net>
2018-10-02 20:18:17 -0400

    added background highlight for menu items hovered over

--- React_practice/StillSea/StillSea_Home.js
+++ React_practice/StillSea/StillSea_Home.js
@@ -5,7 +5,7 @@ class StillSea_Home extends Component {
     return (
       <div>
         <h2>STILL SEA SPA TEMPLATE</h2>
-        <p>This is a template for creating Single Page Apps</p>
+        <p>This is a template for creating Single Page Apps that use React Router</p>
       </div>
     );
   }
--- React_practice/StillSea/StillSea_Main.js
+++ React_practice/StillSea/StillSea_Main.js
@@ -8,7 +8,40 @@ import StillSea_Home from "./StillSea_Home";
 import StillSea_ImgRotate from "./StillSea_ImgRotate";
 import StillSea_ImgOverlay from "./StillSea_ImgOverlay";
 
-class StillSea_Main extends Component {
+class StillSea_Main extends Component {	
+	
+	constructor() {
+    super();
+    
+    this.state = { 
+		   active_link_0: {color: '#fff'},
+		   active_link_1: {color: '#fff'},
+		   active_link_2: {color: '#fff'}		  
+		};      
+	}
+	
+  setActiveLink(section){
+	  this.setState({
+		    active_link_0: {color: '#fff'},
+			active_link_1: {color: '#fff'},
+			active_link_2: {color: '#fff'}
+	   });
+	   
+	   switch(section){
+		   case 0: 
+		      this.setState({ active_link_0: {color: '#0f0'} });
+			  break;
+		   case 1: 
+		      this.setState({ active_link_1: {color: '#0f0'} });
+			  break;
+		   case 2: 
+	          this.setState({ active_link_2: {color: '#0f0'} });
+			  break;
+		   default:
+		      alert("Nothing matched in switch");
+	   }	   
+  }
+  
   render() {
     return (
 	  <HashRouter>
@@ -23,9 +56,9 @@ class StillSea_Main extends Component {
 				  <div style={{clear:'both'}}></div>
 			</div>    
           <div className="menu-bar">
-            <div className="menu-item"><NavLink exact to="/">Home</NavLink></div>
-            <div className="menu-item"><NavLink to="/StillSea_ImgRotate">Section 1</NavLink></div>
-            <div className="menu-item"><NavLink to="/StillSea_ImgOverlay">Section 2</NavLink></div>
+            <div className="menu-item" onClick={() => this.setActiveLink(0)} ><NavLink exact to="/"style={this.state.active_link_0}>Home</NavLink></div>
+            <div className="menu-item" onClick={() => this.setActiveLink(1)}><NavLink to="/StillSea_ImgRotate" style={this.state.active_link_1}>Section 1</NavLink></div>
+            <div className="menu-item" onClick={() => this.setActiveLink(2)}><NavLink to="/StillSea_ImgOverlay" style={this.state.active_link_2}>Section 2</NavLink></div>
 			<div style={{clear:'both'}}></div>
           </div>
           <div className="content">
