43b338e76e856705e7a79ce86b9bdf963e057d5d
KatherineB <kbiefer@golden.net>
2018-10-12 15:59:14 -0400

    got the JSON data to load and display in the table

--- React_practice/StillSea/StillSea_JSON_load.js
+++ React_practice/StillSea/StillSea_JSON_load.js
@@ -5,62 +5,22 @@ class StillSea_JSON_load extends Component {
     super();
     
     this.state = { 
-	       data: null
+		   planetaryData2: []	
 		};  
-       
-/*	   	fetch('data/planets.json')
-  .then(function(response) {
-    console.log(`status: ${response.status}`);
-    console.dir(response.body);
-    return response.json() 
-  })*/
- 
-
-	   
 	}
-
-	/*fetch('data/planets2.json')
-  .then(function(response) {
-    return response.json();
-  }) 
- .then(function(planetData) {
-     alert('test');
-     alert(planetData[4].planet);
-      document.write(
-      `Planet: ${planetData[5].planet} <br/>
-      Distance from Sun: ${planetData[5].distFromSun} <br />
-      Diameter: ${planetData[5].diameter} `
-    );
-  }); */
-  
-  
-
+	
  componentDidMount() {
-      /*      fetch('data/planets2.json')
-      .then(response => response.json())
-      .then(data => this.setState({ data })); */
-	   
+	 let index = -1;
+     fetch('planets2.json')
+     .then(response => response.json())
+	 .then(planetaryData2 => this.setState({ planetaryData2 }));	
  }	  
-	 
-  
+	  
   render() {
-	 let planetaryData = [{
-		 "planet": "Mercury",
-		 "diameter": 4880,
-		 "distFromSun": 57.9,
-		 "numSatellites": 0,
-		 "revTime": "88 days"
-		},
-		{
-		 "planet": "Venus",
-		 "diameter": 12100,
-		 "distFromSun": 108.2,
-		 "numSatellites": 0,
-		 "revTime": "224.7 days"
-		}]; 
-		let index = -1;
-    return (
-	     
+	const {planetaryData2} = this.state; 	 
+	let index = -1;
+		
+    return (  
         <div className="section-3">
 		    <div className="section-title">Planets</div>
 			
@@ -72,35 +32,19 @@ class StillSea_JSON_load extends Component {
 			  <th>Number of Satellites</th>
 			  <th>Time to Revolve Around Sun</th>
 			</tr>
-			<tr>
-			   <td>{planetaryData[0].planet} </td>
-			   <td>{planetaryData[0].diameter} </td>	
-               <td>{planetaryData[0].distFromSun} </td>
-               <td>{planetaryData[0].numSatellites} </td>
-               <td>{planetaryData[0].revTime} </td>			   
-			</tr>
-			<tr>
-			   <td>{planetaryData[1].planet} </td>
-			   <td>{planetaryData[1].diameter} </td>	
-               <td>{planetaryData[1].distFromSun} </td>
-               <td>{planetaryData[1].numSatellites} </td>
-               <td>{planetaryData[1].revTime} </td>			   
-			</tr>
 			 {		
-			    planetaryData.map(item => {
+			   planetaryData2.map(item => {
 					index++;
-					return <tr><td>{planetaryData[index].planet}</td>
-					<td>{planetaryData[index].diameter}</td>
-					<td>{planetaryData[index].distFromSun}</td>
-					<td>{planetaryData[index].numSatellites}</td>
-					<td>{planetaryData[index].revTime}</td>
+					return <tr><td>{planetaryData2[index].planet}</td>
+					<td>{planetaryData2[index].diameter}</td>
+					<td>{planetaryData2[index].distFromSun}</td>
+					<td>{planetaryData2[index].numSatellites}</td>
+					<td>{planetaryData2[index].revTime}</td>
 					</tr>
-				})
-			  }
-			
-			</table>
-			
-			  
+				}) 
+			  }  
+			</table>  
+						  
         </div>		
     );
   }
