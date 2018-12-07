b9f40685d5e9ebf725ae28c582e72b95aaeb5133
KatherineB <kbiefer@golden.net>
2018-12-07 16:04:32 -0500

    decided on format of JSON file

--- PHP_practice/clubMemberInfo.php
+++ PHP_practice/clubMemberInfo.php
@@ -1,32 +1,43 @@
 <?php
-   /* 
-	echo $_GET['top'];
-	echo $_GET['deep'];*/
+    print_r($_GET);
 	
-	/*print_r($_GET);*/
+	echo "<br />";
 	
-	$file = "members.json";
-	$top = $_GET['top'];
-	$deep = $_GET['deep'];
+	$file = "clubmemberinfo.json";
+	$id_number = $_GET['mid'];
+	$first_name = $_GET['fn'];
+	$last_name = $_GET['ln'];
 	
 	/*echo $file;*/
-	echo $top;
-	echo $deep;
+	echo $id_number;
+	echo $first_name;
+	echo $last_name;
+	
+	echo "<br />";
 	
 	$str = file_get_contents($file);
 	$json = json_decode($str, true);
 	
-    $memberCount = count($json['members']);
+
+	
+    $memberCount = count($json['clubmemberinfo']);
 	echo $memberCount;
 	
-	/*echo '<pre>' . print_r($json, true) . '</pre>';*/
+	echo "<br />";
+	
+	//echo '<pre>' . print_r($json, true) . '</pre>';
 	
 	for($i=0;$i<$memberCount;$i++){
-	   $data = $json['members'][$i+1][$deep];
-	   echo $data;
+       if($id_number == $json['clubmemberinfo'][$i]["id_number"]){
+		   echo $json['clubmemberinfo'][$i]['first_name'] . ' ';
+		   echo $json['clubmemberinfo'][$i]['last_name'] . "<br />";
+		   echo $json['clubmemberinfo'][$i]['year_joined'];	   
+	   }  
 	}
 	
-	echo $json['members'][1]['year_joined'];
+	echo "<br />";
+
+    print_r($json['clubmemberinfo'][0]['involvement']);
 
 	 
 ?>
