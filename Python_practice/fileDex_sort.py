ee93924afd47543ff165d8018bf4bbe74f5e32b0
KatherineB <kbiefer@golden.net>
2019-09-20 17:19:09 -0400

    changed code to prevent blank lines appearing in the output file

--- Python_practice/fileDex_sort.py
+++ Python_practice/fileDex_sort.py
@@ -9,9 +9,10 @@ with open("filedex_newData.txt","r") as inputFile:
 
 employees.sort()
 
-with open("filedex_sorted.txt","w") as sortedfile:
-    for element in employees:
-        print(element, file=sortedfile)
-
-
-
+with open("filedex_sorted.txt","w") as sortedfile:   
+    i = 0
+    while i < len(employees):
+        if i < len(employees) - 1:
+            employees[i] = employees[i][:-1] #remove newline chars without losing final } from last line
+        print(employees[i], file=sortedfile)
+        i += 1
